aa2f4204f3d4e51996f285e0f71e8a0efb60373d
mattibun <mattibun@gmail.com>
2019-09-15 18:03:53 -0400

    Base game logic complete

--- tic.js
+++ tic.js
@@ -55,11 +55,14 @@ const displayController = (() => {
 const logicController = (() => {
     const player = Symbol("player")
     const computer = Symbol("computer")
+    const noWin = Symbol("no winner yet")
+    const cat = Symbol("cat game")
     let currTurn = player;
     let playSymbols = {
         player: "X",
         computer: "O"
     }
+    let result = noWin;
 
     const changePlayer = () => {
         (currTurn === player) ? currTurn = computer : currTurn = player;
@@ -73,24 +76,50 @@ const logicController = (() => {
     }
 
     const squareClicked = (x, y) => {
-        let spotClicked = findSquare(x,y).square;
-        console.log(spotClicked)
-        if (spotClicked.textContent !== "") return;
+        let spotClicked = findSquare(x,y).square;        
+        if (spotClicked.textContent !== "" || result !== noWin) return;
         if (currTurn === player) {
             spotClicked.textContent = playSymbols.player;
         } else {
             spotClicked.textContent = playSymbols.computer;
         }
-        let result = checkForWinner();
-        if (result === false) changePlayer();
+        result = winnerQ(0,1,2)  // check for 3-in-a-row horizontally
+        ||  winnerQ(3,4,5) 
+        ||  winnerQ(6,7,8) 
+        ||  winnerQ(0,3,6)  // check for 3-in-a-row vertically
+        ||  winnerQ(1,4,7) 
+        ||  winnerQ(2,5,8) 
+        ||  winnerQ(0,4,8)  // check for 3-in-a-row diagonally
+        ||  winnerQ(6,4,2)
+        ||  stalemateQ()
+        || noWin;
+        
+        
+        
+        if (result === noWin) { changePlayer() }
+        else {
+            console.log(result)
+        };
     }
 
-    const checkForWinner = () => {
-        let board = GameBoard.board;
-        let boardValues = [];
+    const winnerQ = (p1, p2, p3) => {
+        let s = (GameBoard.board.map(item => item.square.textContent)) 
+        if (s[p1] === "") return false
+        if (s[p1] !== s[p2]) return false
+        if (s[p1] !== s[p3]) return false
+        let playSymbols = {
+            player: "X",
+            computer: "O"
+        }
 
+        return (s[p1] === playSymbols.player)?player:computer;
+    }
 
+    
 
+    const stalemateQ = () => {
+        let s = (GameBoard.board.map(item => item.square.textContent))
+        if (s.every(item => item !== "")) return cat;
     }
 
     return {
